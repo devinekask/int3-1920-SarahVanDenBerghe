37716ea66aa135bd741f62cc481e497c71ae62e3
Sarah Van Den Berghe <sarahvandenberghe@MacBook-Pro-van-Sarah.local>
2020-01-11 15:51:50 +0100

    Added form for filter option closing #18

--- src/view/pages/webshop.php
+++ src/view/pages/webshop.php
@@ -8,4 +8,35 @@
       <a class="button button--primary" href="#"><span>Ontdek</span></a>
     </div>
   </article>
+
+  <article>
+    <section class="form__wrapper">
+      <h2 class="hidden">Filter webshop items</h2>
+      <form action="index.php?page=webshop">
+        <div class="filter">
+          <p class="filter__title">Categorieën</p>
+          <label for="abonnementen" class="filter__label">
+            <input class="filter__input" type="checkbox" id="abonnementen" name="categories[]" value="abonnementen">
+            <span class="checkbox__mark"></span>
+            Abonnementen
+          </label>
+          <label for="gadgets" class="filter__label">
+            <input class="filter__input" type="checkbox" id="gadgets" name="categories[]" value="gadgets">
+            <span class="checkbox__mark"></span>
+            Gadgets
+          </label>
+          <label for="boeken" class="filter__label">
+            <input class="filter__input" type="checkbox" id="boeken" name="categories[]" value="boeken">
+            <span class="checkbox__mark"></span>
+            Boeken
+          </label>
+          <p class="filter__delete">Filter wissen</p>
+        </div>
+      </form>
+    </section>
+
+    <section>
+
+    </section>
+  </article>
 </div>
