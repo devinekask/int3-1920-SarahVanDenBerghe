c082d85a31bb5d4011f520c584c83135cb8cb917
Sarah Van Den Berghe <sarahvandenberghe@MacBook-Pro-van-Sarah.local>
2020-01-11 03:30:51 +0100

    Navbar in HTML closed #13

--- src/view/layout.php
+++ src/view/layout.php
@@ -7,6 +7,40 @@
     <?php echo $css;?>
   </head>
   <body>
+
+    <header>
+      <div class="container">
+        <nav class="menu menu--secondary">
+          <ul class="menu__items">
+            <li class="menu__item">Video</li>
+            <li class="menu__item">TV-gids</li>
+            <li class="menu__item">Zoekertjes</li>
+            <li class="menu__item">Abonnement nemen</li>
+          </ul>
+          <ul class="menu__items">
+            <li class="menu__item">Webshop</li>
+            <li class="menu__item">Nu in Humo</li>
+            <li class="menu__item">Login</li>
+            <li class="menu__item">Registreer</li>
+          </ul>
+        </nav>
+      </div>
+      <nav class="menu menu--primary">
+        <div class="container">
+          <ul class="menu__items">
+            <li class="menu__item">Home</li>
+            <li class="menu__item">Actua</li>
+            <li class="menu__item">Humor</li>
+            <li class="menu__item">Logo</li>
+            <li class="menu__item">TV/Film</li>
+            <li class="menu__item">Muziek</li>
+            <li class="menu__item">Boeken</li>
+            <li class="menu__item">Winkelmand</li>
+            <li class="menu__item">Zoek</li>
+          </ul>
+        </div>
+      </nav>
+    </header>
     <main>
       <?php
         if(!empty($_SESSION['error'])) {
@@ -16,7 +50,6 @@
           echo '<div class="info box">' . $_SESSION['info'] . '</div>';
         }
       ?>
-      <header><h1>Todos - <?php echo $title; ?></h1></header>
       <?php echo $content;?>
     </main>
     <?php echo $js; ?>
