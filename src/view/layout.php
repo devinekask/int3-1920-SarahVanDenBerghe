c5386e148b556a39ac5c24687a8a0d50b4d54147
Sarah Van Den Berghe <sarah.van.den.berghe@student.howest.be>
2020-01-15 21:53:07 +0100

    stylelint problemen opgelost

--- src/view/layout.php
+++ src/view/layout.php
@@ -2,9 +2,9 @@
 <html>
   <head>
     <meta charset="utf-8">
-    <meta name=“viewport” content=“initial-scale=1.0, width=device width” />
+    <meta name="viewport" content="initial-scale=1.0" width=device width” />
+    <link rel="shortcut icon" href="assets/img/favicon.jpg"/>
     <title><?php echo $title; ?></title>
-    <?php /* NEW */ ?>
     <?php echo $css;?>
   </head>
   <body>
