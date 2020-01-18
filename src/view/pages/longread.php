d5da76dc17de00d2cc15ac8fcfb004cd0bd229c1
Sarah Van Den Berghe <sarah.van.den.berghe@student.howest.be>
2020-01-18 14:26:29 +0100

    Responsive aangepast, longread content toegevoegd

--- src/view/pages/longread.php
+++ src/view/pages/longread.php
@@ -1,4 +1,31 @@
-<!-- longread -->
-<h1 class="title title--longread">Fahrenheit 451</h1>
-<p>Ray Bradbury</p>
-<a class="button button--longread">Bestel boek</a>
+<!-- INTRO -->
+<span class="line line--intro"></span>
+<article class="intro">
+  <img src="../../assets/img/longread/header_820x820.png" alt="">
+  <div>
+    <h1 class="intro__title">Fahrenheit 451</h1>
+    <p class="intro__author">Ray Bradbury</p>
+    <a class="intro__button">Bestel boek</a>
+  </div>
+</article>
+
+<div class="quote">
+  <p>Ontdek de verborgen geheimen</p>
+</div>
+
+<!-- BOOK SUMMARY -->
+<div class="container--small">
+  <article class="summary">
+    <p class="summary__opening">De internationaal bekroonde roman van Ray Bradbury, Fahrenheit 451, is een meesterwerk van de twintigste-eeuwse literatuur en speelt zich af in een sombere dystopische toekomst.</p>
+    <p class="summary__book">Guy Montag is brandweerman. In zijn wereld, waar de televisie heerst en literatuur nagenoeg is uitgestorven, blussen brandweermannen geen vuren: ze beginnen ze juist. Het is zijn baan om de meest illegale artikelen te verwoesten: boeken. Ook de huizen waarin die boeken zijn verborgen, gaan in vlammen op. Montag twijfelt nooit aan de vernietiging en verwoesting die zijn acties teweegbrengen, en keert aan het eind van de dag altijd vrolijk terug naar zijn saaie leven en zijn vrouw, Mildred, die de hele dag met haar “familie” op televisie doorbrengt. Maar dan ontmoet hij zijn excentrieke jonge buurvrouw, Clarisse, die hem introduceert tot een verleden waar mensen niet in angst leefden en tot een heden waar men de wereld ziet door de ideeën in boeken in plaats van het hersenloze gepraat op de televisie. Als Mildred probeert zelfmoord te plegen en Clarisse verdwijnt, begint Montag overal vraagtekens bij te zetten: vooral bij wat hij nooit heeft geweten. Hij begint boeken te verbergen in zijn huis, en als zijn misdaad wordt ontdekt, moet de brandweerman vluchten voor zijn leven.</p>
+    <div class="wrapper">
+      <p class="summary__history">Fahrenheit 451 is geïnspireerd door de vele boekbranding doorheen de Verenigde Staten. De titel ‘Fahrenheit 451’ was dan ook niet willekeurig gekozen, deze verwijst naar de temperatuur waar een boek in brand schiet. Ray Bradbury, de schrijver van dit boek, was ongerust door de boekbranding tijdens de WOII en de censuur van vrije ideeën. Hij zag hoe de opkomst van media de interesse in lezen verminderde en besloot op basis hiervan de internationale bestseller Fahrenheit 451 te schrijven.</p>
+      <div class="summary__img"></div>
+    </div>
+    <!-- <div class="summary__history"> -->
+     <!-- <div class="img__wrapper">
+        <img src="../../assets/img/longread/televisie_860x366.png" alt="">
+      </div>
+    </div> -->
+  </article>
+</div>
