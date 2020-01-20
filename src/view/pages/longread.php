121d9d5ba0ca17a8eda4eeffea1168567b32d65b
Sarah Van Den Berghe <sarah.van.den.berghe@student.howest.be>
2020-01-20 01:49:04 +0100

    Content toegevoegd longread

--- src/view/pages/longread.php
+++ src/view/pages/longread.php
@@ -30,15 +30,75 @@
 
 <!-- AUTHOR -->
 <article class="author line--author">
+  <h2 class="hidden">Auteur</h2>
   <div class="author__quote subtitle">
-    <p>You must stay drunk on writing so reality cannot destroy you.</p>
+    <p>You must stay drunk on writing so reality cannot destroy you</p>
   </div>
   <div class="author__introduction container--small">
     <img class="author__image" src="../../assets/img/longread/auteur_620x528.png" alt="">
     <div class="author__info">
       <p class="subtitle">Auteur</p>
       <p class="title">Ray Bradbury</p>
-      <p>Ray Douglas Bradbury (Waukegan (Illinois), 22 augustus 1920 – Los Angeles, 5 juni 2012) was een Amerikaanse sciencefiction- en fantasyschrijver. Zijn eerste boek, de bundel Dark Carnival, werd in 1947 uitgebracht. Fahrenheit 451 en The Martian Chronicles worden tot zijn belangrijkste werken gerekend. [Inspiratie schrijven van boek] dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam.</p>
+      <p class="info">Ray Douglas Bradbury (Waukegan (Illinois), 22 augustus 1920 – Los Angeles, 5 juni 2012) was een Amerikaanse sciencefiction- en fantasyschrijver. Zijn eerste boek, de bundel Dark Carnival, werd in 1947 uitgebracht. Fahrenheit 451 en The Martian Chronicles worden tot zijn belangrijkste werken gerekend. Bradbury schreef het succesverhaal Fahrenheit 451 na WOII waar in Amerika veel boekbrandingen plaats vond.</p>
     </div>
   </div>
+</article>
+
+<!-- HISTORY -->
+<article>
+  <h2 class="hidden"> Geschiedenis</h2>
+  <ul class="timeline">
+    <div class="items__wrapper">
+    <li class="timeline__item">
+    <p class="subtitle">Library of Ashurbanipal</p>
+    <span>612 BC</span>
+    </li>
+    <li class="timeline__item">
+      <p class="subtitle">Varianten van de koran</p>
+      <span>650</span>
+    </li>
+    <li class="timeline__item">
+      <p class="subtitle">Luther’s duitse bijbel Vertaling</p>
+      <span>1534</span>
+    </li>
+    <li class="timeline__item">
+      <p class="subtitle">Bibliotheek Universiteit Leuven</p>
+      <span>1944</span>
+    </li>
+    </div>
+  </ul>
+  <section class="history container--small">
+    <div class="history__text">
+      <h3 class="title">De harde realiteit</h3>
+      <p>Tot diep in de 19de eeuw keurden geestelijken het lezen door het gewone volk af. Lezen zou lui maken, tijdsverspilling zijn of eenvoudige mensen op de verkeerde ideeën brengen. Vooral romans waren slecht: ze hadden een afstompende werking op het geweten en verslapten de wil. Het verbranden van boeken, geschreven teksten en mediadragers meestal publiekelijk en ritueel verbranden is iets dat doorheen de geschiedenis gebeurde. Het motief voor boekverbranding komt neer op censuur op morele, religieuze en politieke gronden. </p>
+      <p>In sommige gevallen zijn de vernietigde werken onvervangbaar en vormt hun verbranding een ernstig verlies voor cultureel erfgoed. In andere gevallen, zoals de boekverbrandingen van de nazi’s , overleven kopieën van de vernietigde boeken. Het branden van boeken wordt symbolisch voor een hard en onderdrukkend regime gezien, dat een aspect van de heersende cultuur probeert te censureren of tot zwijgen te brengen. Het kan een minachting zijn voor de inhoud van het boek of de auteur, en de handeling is bedoeld om bredere publieke aandacht voor deze mening te vestigen.</p>
+    </div>
+    <ul class="history__examples">
+      <li>
+        <p class="subtitle">612 BC</p>
+        <p>Tijdens een aanval op het Koninklijk Paleis in Nineveh, werd de bibliotheek van Ashurbanipal verbrand.</p>
+      </li>
+      <li>
+        <p class="subtitle">650</p>
+        <p>Varianten van de Koran werden verwoest door de kalifaat Uthman ibn ‘Affan.</p>
+      </li>
+      <li>
+        <p class="subtitle">1534</p>
+        <p>De Duitse vertaling van Martlin Luther werd op vraag van de Paus verbrand.</p>
+      </li>
+      <li>
+        <p class="subtitle">1944</p>
+        <p>Tijdens de Tweede Wereldoorlogen werden veel bibliotheken verwoest, onder andere die van de Universiteit Leuven.</p>
+      </li>
+    </ul>
+  </section>
+  <div class="bookburning">
+    <div class="line--bookburning"></div>
+  </div>
+</article>
+
+<!-- HEDENDAAGS -->
+<article class="present container--small">
+  <h2 class="title">Een hedendaags probleem</h2>
+  <p>Het verbranden van boeken beperkt zich niet in onze geschiedenis. In de moderne tijd zijn andere vormen van media, zoals grammofoonplaten, videobanden en cd ’s ook gebrand, versnipperd of verpletterd. Het verbranden van boeken was een hulpmiddel dat werd gebruikt voor religieuze of autoritaire redenen zodat tegenstrijdige opvattingen onderdrukt worden. Alhoewel we het verbranden van boeken in de 21ste eeuw in vele democratische landen niet snel tegenkomen, speelt censuur en onderdrukking op de dag van vandaag nog steeds een grote rol. Dit gebeurt via andere media en beperkt zich niet tot boeken. Het verbannen van websites, stopzetten van televisiezenders en andere mediakanalen gebeurt nog steeds in landen zoals China, Rusland, Pakistan, enzovoort. Het ‘verbranden’ van informatie en vrije meningsuiten wordt op de dag vandaag, bij wijze van spreken, nog steeds voortgezet. </p>
 </article>
